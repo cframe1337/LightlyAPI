1c1cd99672a1f0dd9b652884782d2216199305b1
cframe1337 <110549027+cframe1337@users.noreply.github.com>
2023-07-26 00:42:26 +0300

    Update libLightlyApi.lua

--- Lightly API 1.6 test/libLightlyApi.lua
+++ Lightly API 1.6 test/libLightlyApi.lua
@@ -1,4 +1,4 @@
 local LightlyApi = {
     get = loadfile("https://raw.githubusercontent.com/cframe1337/LightlyAPI/main/Lightly%20API%201.6%20test/libLightlyApiRead.lua"),
-    write = loadfile("libLightlyApiWrite.lua"),
+    write = loadfile("https://raw.githubusercontent.com/cframe1337/LightlyAPI/main/Lightly%20API%201.6%20test/libLightlyApiWrite.lua"),
 }
