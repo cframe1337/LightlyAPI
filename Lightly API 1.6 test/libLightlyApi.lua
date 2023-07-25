0e5f1bb626ddf551771774a16a12e52683dec524
cframe1337 <110549027+cframe1337@users.noreply.github.com>
2023-07-26 00:38:58 +0300

    Update libLightlyApi.lua

--- Lightly API 1.6 test/libLightlyApi.lua
+++ Lightly API 1.6 test/libLightlyApi.lua
@@ -1,15 +1,3 @@
---[[
-    Lightly API v1.5
-
-    # Updated at 11.07.2023 (0:00)
-
-    - Added actions (write/raead)
-    - Added RVT
-
-    Lightly API GitHub: https://github.com/cframe1337/LightlyAPI
-    Lightly API Creator Telegram: @hackthread
-]]--
-
 local LightlyApi = {
     get = loadfile("libLightlyApiGet.lua"),
     write = loadfile("libLightlyApiWrite.lua"),
